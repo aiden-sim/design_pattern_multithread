0eda3169622ccfc9cfb5753123fa585e53cbe7fc
simjunbo <s860620@gmail.com>
2019-02-25 23:34:56 +0900

    single

---
+++ src/main/java/singlethreaedexecution/example1/Main.java
@@ -0,0 +1,16 @@
+package singlethreaedexecution.example1;
+
+/**
+ * 예제1) Single Threaded Execution 패턴을 사용해야 함에도 불구하고 사용하지 않는 프로그램
+ * 잘못된 예제
+ * 문을 만들고, 3명을 문으로 통과 시키는 클래스
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Testing Gate, hit CTRL + C to exit.");
+		Gate gate = new Gate();
+		new UserThread(gate, "Alice", "Alaska").start();
+		new UserThread(gate, "Bobby", "Brazil").start();
+		new UserThread(gate, "Chris", "Canada").start();
+	}
+}
---
+++ src/main/java/thread/example/Something.java
@@ -0,0 +1,35 @@
+package thread.example;
+
+public class Something {
+	public void iA() {
+
+	}
+
+	public void iB() {
+
+	}
+
+	public synchronized void iSyncA() {
+
+	}
+
+	public synchronized void iSyncB() {
+
+	}
+
+	public static void cA() {
+
+	}
+
+	public static void cB() {
+
+	}
+
+	public static synchronized void cSyncA() {
+
+	}
+
+	public static synchronized void cSyncB() {
+
+	}
+}
